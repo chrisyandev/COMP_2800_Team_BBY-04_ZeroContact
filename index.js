965bb07236ec7d8e102045397d0840c26264a23a
mmaeschalck <matthew@Matthews-MacBook-Pro-2.local>
2020-05-14 14:07:01 -0700

    node server fixes

--- index.js
+++ index.js
@@ -14,9 +14,10 @@ MongoClient.connect(connectionString, {useUnifiedTopology: true}).then(client =>
     app.use(express.static("public"));
     app.use(express.json());
     app.set("view engine", "ejs");
-
-    app.get("/", (req, res) => res.render("pages/zero-contact/main.ejs"));
-    
+    app.get("/", (req, res) => res.render("pages/landing-page/home.ejs"));
+    app.get("/about", (req, res) => res.render("pages/landing-page/about.ejs"));
+    app.get("/game", (req, res) => res.render("pages/zero-contact/main.ejs"));
+    app.get("/minigame", (req, res) => res.render("pages/zero-contact/minigame.ejs"));
     app.listen(3000);
 })
 .catch(console.error);
