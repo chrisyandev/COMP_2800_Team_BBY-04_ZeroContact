b2828d1083e7bbe97232f85fdb189c5a40adeba1
Gabriel Baluyut <60728482+gabeBaluyut@users.noreply.github.com>
2020-05-15 15:56:21 -0700

    Added footer with associated links

--- index.js
+++ index.js
@@ -29,6 +29,10 @@ MongoClient.connect(connectionString, {
     app.get("/signup", (req, res) => res.render("pages/landing-page/signup.ejs"));
     app.get("/login", (req, res) => res.render("pages/landing-page/login.ejs"));
     app.get("/delete", (req, res) => res.render("pages/landing-page/home.ejs"));
+    app.get("/error", (req, res) => res.render("pages/landing-page/error.ejs"));
+    app.get("/privacy-policy", (req, res) => res.render("pages/landing-page/privacy.ejs"));
+    app.get("/attributions", (req, res) => res.render("pages/landing-page/attributions.ejs"));
+    app.get("/contact-us", (req, res) => res.render("pages/landing-page/contact.ejs"));
 
     app.get("/leaderboard", (req, res) => {
         usersCollection.find().sort({days: -1}).toArray().then((highscores) => {
