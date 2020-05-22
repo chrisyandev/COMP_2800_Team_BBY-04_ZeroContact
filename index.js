d011fb727b81f7dc1882e27c31860a9d49e3b4a2
mmaeschalck <matthew@Matthews-MacBook-Pro-2.local>
2020-05-22 14:24:48 -0700

    Indexjs cleanup and css changes

--- index.js
+++ index.js
@@ -157,53 +157,6 @@ MongoClient.connect(connectionString, {
             }).catch(error => console.error(error));
         })
 
-
-        app.get("/", (req, res) => res.render("pages/landing-page/home.ejs"));
-        app.get("/about", (req, res) => res.render("pages/landing-page/about.ejs"));
-        app.get("/signup", (req, res) => res.render("pages/landing-page/signup.ejs"));
-        app.get("/login", (req, res) => res.render("pages/landing-page/login.ejs"));
-
-        app.post('/signup', (req, res) => {
-            bcrypt.genSalt(saltRounds).then(salt => {
-                console.log("salt " + salt)
-                return bcrypt.hash(req.body.password, salt)
-            }).then(hash => {
-                console.log("hash " + hash)
-                usersCollection.insertOne({
-                    username: req.body.username,
-                    password: hash,
-                    days: 0
-                })
-            }).catch(err => console.error(err.message))
-            res.render("pages/landing-page/login.ejs")
-        })
-
-        app.post("/login", (req, res) => {
-            usersCollection.find({
-                username: req.body.username
-            }).toArray().then((user) => {
-                console.log(user)
-                console.log("Attempting login")
-                if (user.length == 0) {
-                    res.redirect("/");
-                    console.log("User not found")
-                } else {
-                    bcrypt.compare(req.body.password, user[0].password, (err, result) => {
-                        console.log(result);
-
-                        if (result == true) {
-                            res.render("pages/landing-page/profile.ejs", {
-                                username: req.body.username,
-                                user: user
-                            })
-                        } else {
-                            console.error(err);
-                        }
-                    })
-                }
-            }).catch((error) => console.error(error))
-        })
-
         app.put("/longest-days", (req, res) => {
             usersCollection.find({
                 username: req.body.username
